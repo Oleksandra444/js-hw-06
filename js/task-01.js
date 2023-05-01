098d455e470530335ea20725632a9c56e6e542c0
Oleksandra444 <oleksandra.work.4.4.4@gmail.com>
2023-05-01 11:30:47 +0400

    hw-06v3

--- js/task-01.js
+++ js/task-01.js
@@ -8,7 +8,7 @@ console.log(`Number of categories: ${items.length}`);
 
 items.forEach(item => { 
     console.log(`Category: ${item.firstElementChild.textContent}`);
-    // console.log(item.firstElementChild.textContent);
+    
     console.log(`Elements: ${item.querySelectorAll('li').length}`);
 
 })
